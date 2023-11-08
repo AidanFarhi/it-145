ccf4bbdeed4dcead920bb665872222f57ac0a5a0
Aidan Farhi <aidanmcfarhi@gmail.com>
2023-11-08 04:34:19 -0500

    finished DrawHalfArrow lab

--- labs/DrawHalfArrow.java
+++ labs/DrawHalfArrow.java
@@ -3,10 +3,32 @@ import java.util.Scanner;
 public class DrawHalfArrow {
 
     /**
-     * Prints out the base of a half-arrow to output based on given dimensions.
+     * Gets dimensions for a half arrow from input.
      * 
-     * @param baseHeight the height of the base
-     * @param baseWidth  the width of the base
+     * @param scnr a Scanner object
+     * @return     an array of ints that contains the dimensions
+     */
+    public static int[] getArrowDimensions(Scanner scnr) {
+        int[] dimensions = new int[3];
+        System.out.println("Enter arrow base height:");
+        dimensions[0] = scnr.nextInt();
+        System.out.println("Enter arrow base width:");
+        dimensions[1] = scnr.nextInt();
+        // Keep prompting for input until the arrowHeadWidth
+        // is wider than arrowBaseWidth.
+        while (dimensions[2] <= dimensions[1]) {
+            System.out.println("Enter arrow head width:");
+            dimensions[2] = scnr.nextInt();
+        }
+        System.out.println("");
+        return dimensions;
+    }
+
+    /**
+     * Prints out the base of a half arrow to output based on the given dimensions.
+     * 
+     * @param baseHeight the height of the half arrow base
+     * @param baseWidth  the width of the half arrow base
      */
     public static void drawArrowBase(int baseHeight, int baseWidth) {
         for (int i = 0; i < baseHeight; i++) {
@@ -18,9 +40,9 @@ public class DrawHalfArrow {
     }
 
     /**
+     * Prints out the head of a half arrow to output based on a given dimension.
      * 
-     * 
-     * @param arrowHeadWidth
+     * @param arrowHeadWidth the width of the half arrow head
      */
     public static void drawArrowHead(int arrowHeadWidth) {
         for (int i = 0; i < arrowHeadWidth; i++) {
@@ -32,25 +54,15 @@ public class DrawHalfArrow {
     }
 
     public static void main(String[] args) {
-        
-        // initialize a Scanner and variables to store half-arrow dimensions
+        // Initialize a Scanner to obtain dimensions from input.
         Scanner scnr = new Scanner(System.in);
-        int arrowBaseHeight = 0;
-        int arrowBaseWidth = 0;
-        int arrowHeadWidth = 0;
-        
-        // get arrow base dimensions from input
-        System.out.println("Enter arrow base height:");
-        arrowBaseHeight = scnr.nextInt();
-        System.out.println("Enter arrow base width:");
-        arrowBaseWidth = scnr.nextInt();
-
-        while (arrowHeadWidth <= arrowBaseWidth) {
-            System.out.println("Enter arrow head width:");
-            arrowHeadWidth = scnr.nextInt();
-        }
-        System.out.println("");
-        
+        // Call a method to get dimetions from input.
+        int[] dimensions = getArrowDimensions(scnr);
+        // Store the dimensions from the array into individual variables.
+        int arrowBaseHeight = dimensions[0];
+        int arrowBaseWidth = dimensions[1];
+        int arrowHeadWidth = dimensions[2];
+        // Call methods to draw the base and head of the half arrow.
         drawArrowBase(arrowBaseHeight, arrowBaseWidth);
         drawArrowHead(arrowHeadWidth);
     }
