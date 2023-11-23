a1e7bedf0438602841795c8e33dd3adc27e4570c
Aidan Farhi <aidanmcfarhi@gmail.com>
2023-11-23 17:57:33 -0500

    renamed directories and added module 5 milestone dir

---
+++ module-5-milestone/Monkey.java
@@ -0,0 +1,40 @@
+public class Monkey extends RescueAnimal{
+
+    private double tailLength;
+    private double height;
+    private double bodyLength;
+    private String species;
+
+
+    public double getTailLength() {
+        return tailLength;
+    }
+
+    public void setTailLength(double tailLength) {
+        this.tailLength = tailLength;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    public double getBodyLength() {
+        return bodyLength;
+    }
+
+    public void setBodyLength(double bodyLength) {
+        this.bodyLength = bodyLength;
+    }
+
+    public String getSpecies() {
+        return species;
+    }
+
+    public void setSpecies(String species) {
+        this.species = species;
+    }
+}
