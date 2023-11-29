eca507e03eaa85db48934ec7ea7f07d2ed549eaa
Aidan Farhi <aidanmcfarhi@gmail.com>
2023-11-29 11:49:32 -0500

    implemented input validation

--- module-6-assignment/Paint1.java
+++ module-6-assignment/Paint1.java
@@ -1,8 +1,10 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Paint1 {
 
     public static void main(String[] args) {
+
         Scanner scnr = new Scanner(System.in);
         double wallHeight = 0.0;
         double wallWidth = 0.0;
@@ -11,16 +13,26 @@ public class Paint1 {
         
         final double squareFeetPerGallons = 350.0;
         
-        // Implement a do-while loop to ensure input is valid
-        // Prompt user to input wall's height
-        System.out.println("Enter wall height (feet): ");
-        wallHeight = scnr.nextDouble();
+        // Loop until a valid height is entered.
+        do {
+            try {
+                System.out.println("Enter wall height (feet): ");
+                wallHeight = scnr.nextDouble();
+            } catch (InputMismatchException e) {
+                scnr.next();
+            };
+        } while (wallHeight <= 0.0);
 
-        // Implement a do-while loop to ensure input is valid
-        // Prompt user to input wall's width
-        System.out.println("Enter wall width (feet): ");
-        wallWidth = scnr.nextDouble();
-
+        // Loop until a valid width is entered.
+        do {
+            try {
+                System.out.println("Enter wall width (feet): ");
+                wallWidth = scnr.nextDouble();
+            } catch (InputMismatchException e) {
+                scnr.next();
+            };
+        } while (wallWidth <= 0.0);
+        
         // Calculate and output wall area
         wallArea = wallHeight * wallWidth;
         System.out.println("Wall area: " + wallArea + " square feet");
